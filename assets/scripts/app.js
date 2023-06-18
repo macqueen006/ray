8a5d0e0f41c2b9d74d08c93c6cde686085886f6b
godwin <65357127+macqueen006@users.noreply.github.com>
2023-06-18 22:16:32 +0100

    minor update
    changes to the navigation and few element

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,24 @@
-"use strict"
-const get = (selector) => document.querySelector(selector);
+"use strict";
+{
+    (function () {
+        const get = (selector) => document.querySelector(selector);
+        const hamburger_menu = get('.menu');
+        const navigation = get('.main-nav');
+
+        hamburger_menu.addEventListener('click', toggleNavigation);
+        document.addEventListener('scroll', () => {
+            const header = get(".header-wrapper");
+            if (window.scrollY > 0) {
+                header.classList.remove('lg:py-8');
+            } else {
+                header.classList.add('lg:py-8');
+            }
+        });
+
+        function toggleNavigation(e) {
+            e.stopImmediatePropagation();
+            navigation.classList.toggle('hidden');
+            navigation.classList.toggle('mobile_nav');
+        }
+    }());
+}
